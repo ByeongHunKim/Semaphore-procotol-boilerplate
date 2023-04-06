6783bfed1acde5415a0bd576ae8e9ed9a57c1fda
ByeongHunKim <rkwhr912@naver.com>
2023-04-06 18:21:24 +0900

    feat : contract deploy on Polygon Mumbai testnet
    폴리곤 뭄바이 테스트넷에 배포 후 적용

--- apps/contracts/contracts/Feedback.sol
+++ apps/contracts/contracts/Feedback.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@semaphore-protocol/contracts/interfaces/ISemaphore.sol";
+import "./ISemaphore.sol";
 
 contract Feedback {
     ISemaphore public semaphore;
